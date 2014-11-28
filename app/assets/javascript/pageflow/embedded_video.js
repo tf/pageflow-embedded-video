cac3cf5b735e124f55da0779254d7de4b8063d74
Stefan Schoettelndreyer <sschoettelndreyer@codevise.de>
2014-11-28 11:48:33 +0100

    applied fix in froogaloop lib to prevent postMessage errors to occur on page changes
    Changing Pages with embedded Vimeo videos resulted in postMessage errors. Applied
    https://github.com/Hacquebord-Buckles/player-api/commit/117c78424db09bf0f5650228b1f96c27d3ca9ae5
    to stop those errors to occur.

--- app/assets/javascript/pageflow/embedded_video.js
+++ app/assets/javascript/pageflow/embedded_video.js
@@ -1,6 +1,6 @@
 //= require_self
 //= require ./embedded_video/page_type
 //= require ./URI.js
-//= require ./froogaloop.min.js
+//= require ./froogaloop.js
 
 pageflow.embeddedVideo = pageflow.embeddedVideo || {};
